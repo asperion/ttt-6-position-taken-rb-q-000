43a80e99786506accfd33a810615b01cebba4dc7
asperion <brownster999@yahoo.com>
2016-02-27 04:56:39 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
-# code your #position_taken? method here!
+def position_taken? (board, position)
+  test = /^[xo]$/i
+  if board[position] =~ test
+    return true
+  else
+    return false
+  end
+end
